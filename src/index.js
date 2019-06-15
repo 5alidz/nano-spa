0e9e20f27e9a22ecbbcea120740b391dfa8b2a55
5alidz <kzack-dev@hotmail.com>
2019-06-15 21:45:50 +0300

    fix exports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import render from './create_element.js'
-import router from './router.js'
-
+import _render from './create_element.js'
+import _router from './router.js'
 import create_dom_nodes from './create_dom_nodes.js'
+
 import { handlers } from './render_route.js'
 
 
-export default {render, router, to_dom: create_dom_nodes.bind(handlers)}
+export const render = _render
+export const router = _router
+export const to_dom = create_dom_nodes.bind(handlers)
