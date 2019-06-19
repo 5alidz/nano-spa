b8dad6c6df0a4d5c07ca75e5c68fd9c706e6dde9
5alidz <kzack-dev@hotmail.com>
2019-06-19 11:11:49 +0300

    add everything if usage from index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-export default {}
+import _render from './render.js'
+import _router from './router.js'
+import _add_handler from './add_handler.js'
+import _to_dom from './to_dom.js'
+
+export const render = _render
+export const router = _router
+export const add_handler = _add_handler
+export const to_dom = _to_dom
