009b0dbddb995ef7b619170c6e92423c289da38f
5alidz <kzack-dev@hotmail.com>
2019-06-19 11:26:09 +0300

    remove dynamic imports

--- router.js
+++ router.js
@@ -1,20 +1,16 @@
 import _global from './internal.global.js'
 import render_route from './internal.render_route.js'
-
+import { bind_initial } from './internal.bind_initial.js'
+import { mount_first_head } from './internal.head.js'
 
 export default function router(o) {
   /* save config in global */
   Object.assign(_global, o)
 
   /* handle first time router mounts */
-  if(document.querySelectorAll('.LINK').length > 0) {
-    import('./internal.bind_initial.js').then(m => { m.bind_initial() })
-  }
-  if(_global.head['*']) {
-    import('./internal.head.js').then(m => m.mount_first_head())
-  }
+  if(document.querySelectorAll('.LINK').length > 0) { bind_initial() }
+  if(_global.head['*']) {mount_first_head()}
   render_route()
-
 
   /* saving prev route to execute on route unmount correctly */
   let prev = window.location.pathname
