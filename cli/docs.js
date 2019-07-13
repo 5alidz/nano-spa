b7ffa00d50cb0c86a79cbabdf6526f50f50d7d8b
5alidz <kzack-dev@hotmail.com>
2019-07-13 09:31:24 +0300

    adding docs generation alpha

--- cli/docs.js
+++ cli/docs.js
@@ -1,19 +1,89 @@
 const fs = require('fs')
 const path = require('path')
 const mkdir = require('mkdirp')
+const cp_file = require('cp-file')
 
-const prop_types_JSON = {}
+const gen_json = (prop_types_path, to_path) => {
+  const _JSON_ = {}
+
+  const append_json = files => files.forEach(file => {
+    const name = file.split('.')[0]
+    const _module = require(path.join(prop_types_path, file))
+    _JSON_[name] = _module
+  })
+
+  const write_json = () => fs.writeFile(
+    path.resolve(to_path),
+    JSON.stringify(_JSON_, null, 2),
+    (_err) => {
+      if(_err) console.log(_err)
+      console.log(`created JSON file. at ${to_path}`)
+    }
+  )
+
+  fs.readdir(prop_types_path, (err, files) => {
+    if(err) console.log(err)
+    append_json(files)
+    write_json()
+  })
+}
 
 module.exports = (/*args*/) => {
   const prop_types_path = path.resolve('./node_modules/nano_spa/handlers.props')
-  mkdir(path.resolve('.', 'docs'))
-  // get default handlers.
-  fs.readdir(prop_types_path, (err, files) => {
-    if(err) console.log(err)
-    files.forEach(file => {
-      const name = file.split('.')[0]
-      const _module = require(path.join(prop_types_path, file))
-      console.log(name, _module)
+  const custom_prop_types_path = path.resolve('./handlers.props')
+
+  mkdir(path.resolve('./docs'))
+  mkdir(path.resolve('./docs/static'))
+  mkdir(custom_prop_types_path)
+
+  (async () => {
+    await cp_file(
+      './node_modules/nano_spa/cli/utils/docs_temp/index.html',
+      './docs/index.html'
+    )
+    await cp_file(
+      './node_modules/nano_spa/cli/utils/docs_temp/main.js',
+      './docs/main.js'
+    )
+  })()
+
+  const handlers_path = path.resolve('./handlers')
+  const on_complete = () => {
+    gen_json(custom_prop_types_path, './docs/custom_handlers.json')
+    gen_json(prop_types_path, './docs/handlers.json')
+  }
+
+  fs.readdir(handlers_path, (err, files) => {
+    const without_dirs = files.filter(file => {
+      return fs.lstatSync(path.join(handlers_path, file)).isFile()
+    })
+
+    without_dirs.forEach((file, i) => {
+      if(!fs.existsSync(path.join(custom_prop_types_path, file))) {
+        fs.writeFile(
+          path.join(custom_prop_types_path, file),
+          'module.exports = {}',
+          (err) => {
+            if(err) return console.log(err)
+            console.log(`[ INFO ] could not find prop types for ${file}`)
+            console.log(`created empty prop types in './handlers.props' for ${file}`)
+            if(i === without_dirs.length - 1) {
+              console.log('writing empty prop types complete')
+              on_complete()
+            }
+          }
+        )
+      }
     })
   })
 }
+
+
+
+
+
+
+
+
+
+
--- cli/utils/docs_temp/main.js
+++ cli/utils/docs_temp/main.js
@@ -1,34 +1,33 @@
 import render from 'nano_spa/render.js'
 import to_dom from 'nano_spa/to_dom.js'
 
-const get_hanlders = async () => {
-  const p = await fetch('/handlers.json')
-  const json = p.json()
-  return json
-}
-
-const handlers_nav = data => {
+const with_data = ({ comp }) => {
+  const fetch_data = async () => {
+    const get_default_json = fetch('/handlers.json')
+    const get_custom_json = fetch('/custom_handlers.json')
+    const [def_json, cust_json] = await Promise.all([get_default_json, get_custom_json])
+    return await { ...def_json.json(), ...cust_json.json() }
+  }
   return render`
-    <Box type='ul'>
-      ${data.handlers.map((handler) => render`
-        <li>
-          <Router::link href=${`/${handler}`}>
-            <a>handler</a>
-          <//>
-        </li>
-      `)}
-    </Box>
+    <div>
+      <Promise
+        promise=${fetch_data}
+        render=${comp}
+      />
+    </div>
   `
 }
 
-const app = ({ links }) => render`
+const app = () => render`
   <div padding='1rem'>
-    <Promise promise=${get_hanlders} render=${handlers_nav}/>
-    <Router dir=${(page) => import(`./pages/${page}`)} />
+    <${with_data}
+      comp=${(data) => render`
+        <pre>${JSON.stringify(data, null, 4)}</pre>
+      `}
+    />
   <//>
 `
 
-// <${simple_box} text='Footer'/>
 document
   .getElementById('root')
   .appendChild(to_dom(render`<${app} />`))
