f3cfd7615b89b3734df5d0c6bbbc3bbbc4bd1809
5alidz <kzack-dev@hotmail.com>
2019-07-25 08:01:37 +0300

    fix build conf merge

--- cli/build.js
+++ cli/build.js
@@ -4,16 +4,20 @@ const fs = require('fs')
 const merge = require('webpack-merge')
 const path = require('path')
 
-let webpack_conf = require('./utils/webpack_config.js')
+const webpack_conf = require('./utils/webpack_config.js')
 
 module.exports = (_args) => {
-  if(fs.existsSync(path.resolve('.', 'webpack.prod.js'))) {
-    webpack_conf = merge(require(path.resolve('.', 'webpack.prod.js')), webpack_conf)
-  }
-  const compiler_prod = webpack(webpack_conf({
+
+  let conf = webpack_conf({
     mode: 'production',
     root: _args.src || 'app'
-  }))
+  })
+
+  if(fs.existsSync(path.resolve('.', 'webpack.prod.js'))) {
+    conf = merge(require(path.resolve('.', 'webpack.prod.js')), conf)
+  }
+
+  const compiler_prod = webpack(conf)
 
   compiler_prod.run(logger.log)
 }
